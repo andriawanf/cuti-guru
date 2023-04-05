97c0d5c22e4c9d29e5aae2ef5db3b06c5cefaca7
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2023-04-05 15:28:11 +0700

    applying livewire on cutis page

--- resources/views/guru/home.blade.php
+++ resources/views/guru/home.blade.php
@@ -10,6 +10,7 @@
     @vite('resources/css/app.css')
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
     @livewireStyles
+
 </head>
 
 <body>
--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -2,7 +2,7 @@
     <aside id="logo-sidebar"
         class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
         aria-label="Sidebar">
-        <div class="h-full px-3 pb-6 overflow-y-auto bg-white dark:bg-gray-800">
+        <div class="h-full px-3 pb-6 overflow-y-auto dark:bg-gray-800">
             <ul class="flex flex-col justify-between font-medium font-roboto h-full">
                 <div class="flex flex-col space-y-4">
                     <li>
